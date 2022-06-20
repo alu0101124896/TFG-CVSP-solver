77f532f51df839f68249c58d97d10afea5e27c20
Sergio Tabares <alu0101124896@ull.edu.es>
2022-06-20 14:30:28 +0100

    Third unilevel formulation added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """ This program calculates the optimal solution to the Capacitated Vertex
 Separator Problem (CVSP) on a graph through unilevel and bilevel approaches."""
 
+from math import inf
 from pathlib import Path
 import sys
 
@@ -31,9 +32,9 @@ def main():
     # b_value = (input("b value (default = '3'): ") or 3)
 
     # Temporal non-interactive version
-    file_path = Path("./data/graph2.txt")
-    k_value = 4
-    b_value = 9
+    file_path = Path("./data/graph1.txt")
+    k_value = 3
+    b_value = 3
 
     with open(file_path, "r", encoding="utf-8-sig") as file:
         raw_data = file.read()
@@ -88,7 +89,8 @@ def cvsp(graph: nx.Graph, k_value: int, b_value: int) -> dict[str, list]:
     graph. """
 
     # solution = formulation_1(graph, k_value, b_value)
-    solution = formulation_2(graph, k_value, b_value)
+    # solution = formulation_2(graph, k_value, b_value)
+    solution = formulation_3(graph, k_value, b_value)
 
     return solution
 
@@ -247,27 +249,186 @@ def formulation_2(graph: nx.Graph, k_value: int,
     return None
 
 
+def formulation_3(graph: nx.Graph, k_value: int, b_value: int) -> list[str]:
+    """ Third formulation for an unilevel approach. """
+
+    V = graph.nodes()
+
+    # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    assert isinstance(solver, pywraplp.Solver)
+
+    # Create the integer binary variables ("3c" constraints).
+    x = {}
+    for v in V:
+        x.update({v: solver.IntVar(0, 1, f"{v}")})
+
+    # Add the "3b" constraints.
+    W = subsets(list(V))
+    for w in W:
+        gw = graph.subgraph(w)
+
+        if any((len(cc_nodes) > b_value)
+               for cc_nodes in nx.connected_components(gw)):
+            ow = inf
+
+        else:
+            ow = n_bins_to_pack(gw, b_value)
+
+        if ow > k_value:
+            solver.Add(sum(x[v] for v in w) >= 1)
+
+    print("\nProblem definition:")
+    print("  Number of variables =", solver.NumVariables())
+    print("  Number of constraints =", solver.NumConstraints())
+
+    # Add the "3a" objective function
+    solver.Minimize(sum(x[v] for v in V))
+
+    # Solve the system.
+    status = solver.Solve()
+
+    print("\nAdvanced usage:")
+    print(f"  Problem solved in {solver.wall_time()} milliseconds")
+    print(f"  Problem solved in {solver.iterations()} iterations")
+    print(f"  Problem solved in {solver.nodes()} branch-and-bound nodes")
+
+    # Print and Parse the solution found.
+    if status == pywraplp.Solver.OPTIMAL:
+        solution = []
+
+        for v in V:
+            int_var = x[v]
+            assert isinstance(int_var, pywraplp.Variable)
+
+            if int_var.solution_value() == 1:
+                solution.append(v)
+
+        return solution
+
+    print("The problem does not have an optimal solution.")
+    return None
+
+
+def subsets(nodes: list[str]) -> list[list[str]]:
+    """ Function to get all combinations with the given nodes. """
+
+    if len(nodes) == 0:
+        return [[]]
+
+    x = subsets(nodes[1:])
+    z = [[nodes[0]] + y for y in x] + x
+
+    return z
+
+
+def n_bins_to_pack(graph: nx.Graph, bin_size: int) -> int:
+    """ Function to get the number of bins of size bin_size needed to pack the
+    connected components of the given graph. """
+
+    # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    assert isinstance(solver, pywraplp.Solver)
+
+    # Variables
+    # x[i, j] = 1 if item i is packed in bin j.
+    x = {}
+    for i in graph.nodes():
+        for j in range(len(graph.nodes())):
+            x[(i, j)] = solver.IntVar(0, 1, f"x_{i}_{j}")
+
+    # y[j] = 1 if bin j is used.
+    y = {}
+    for j in range(len(graph.nodes())):
+        y[j] = solver.IntVar(0, 1, f"y[{j}]")
+
+    # Constraints
+    # Each item must be in exactly one bin.
+    for i in graph.nodes():
+        solver.Add(sum(x[i, j] for j in range(len(graph.nodes()))) == 1)
+
+    # The amount packed in each bin cannot exceed its capacity.
+    for j in range(len(graph.nodes())):
+        solver.Add(sum(x[(i, j)] for i in graph.nodes()) <= y[j] * bin_size)
+
+    # Objective: minimize the number of bins used.
+    solver.Minimize(solver.Sum([y[j] for j in range(len(graph.nodes()))]))
+
+    # Solve the system.
+    status = solver.Solve()
+
+    # Print and Parse the solution found.
+    if status == pywraplp.Solver.OPTIMAL:
+        num_bins = 0
+
+        for j in range(len(graph.nodes())):
+            if y[j].solution_value() == 1:
+                bin_items = []
+                bin_weight = 0
+
+                for i in graph.nodes():
+                    if x[i, j].solution_value() > 0:
+                        bin_items.append(i)
+                        bin_weight += 1
+
+                if bin_weight > 0:
+                    num_bins += 1
+
+    else:
+        num_bins = inf
+
+    return num_bins
+
+
 def draw_solution(graph: nx.Graph, solution: dict[str, list]):
     """ Function to draw the graph's solution. """
 
     n_pos_dict = nx.drawing.layout.kamada_kawai_layout(graph)
 
-    nx.draw_networkx(graph,
-                     n_pos_dict,
-                     nodelist=solution["S"],
-                     edgelist=graph.edges(solution["S"]),
-                     node_color="silver",
-                     style="dashed")
-
-    v_edges = [graph.subgraph(vi).edges() for vi in solution["V"]]
-
-    for vi_nodes, vi_edges, color in zip(solution["V"], v_edges, COLORS):
+    if isinstance(solution, dict):
         nx.draw_networkx(graph,
                          n_pos_dict,
-                         nodelist=vi_nodes,
-                         edgelist=vi_edges,
-                         node_color=color,
-                         width=1.5)
+                         nodelist=solution["S"],
+                         edgelist=graph.edges(solution["S"]),
+                         node_color="silver",
+                         style="dashed")
+
+        v_edges = [graph.subgraph(vi).edges() for vi in solution["V"]]
+
+        for vi_nodes, vi_edges, color in zip(solution["V"], v_edges, COLORS):
+            nx.draw_networkx(graph,
+                             n_pos_dict,
+                             nodelist=vi_nodes,
+                             edgelist=vi_edges,
+                             node_color=color,
+                             width=1.5)
+
+    elif isinstance(solution, list):
+        nx.draw_networkx(graph,
+                         n_pos_dict,
+                         nodelist=solution,
+                         edgelist=graph.edges(solution),
+                         node_color="silver",
+                         style="dashed")
+
+        for node in solution:
+            graph.remove_node(node)
+
+        ccc_nodes = [
+            graph.subgraph(cc).copy() for cc in nx.connected_components(graph)
+        ]
+        ccc_edges = [graph.subgraph(cc).edges() for cc in ccc_nodes]
+
+        for cc_nodes, cc_edges, color in zip(ccc_nodes, ccc_edges, COLORS):
+            nx.draw_networkx(graph,
+                             n_pos_dict,
+                             nodelist=cc_nodes,
+                             edgelist=cc_edges,
+                             node_color=color,
+                             width=1.5)
+
+    else:
+        sys.exit("Error: unknown solution format")
 
     plt.show()
 
--- ortools_test.py
+++ ortools_test.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+""" This program is used to test the sample functions of the Google OR-Tools
+guides and learn the implemented functionalities. """
 
 from ortools.linear_solver import pywraplp
 
@@ -7,7 +9,8 @@ from ortools.linear_solver import pywraplp
 def main():
     """ Main function. """
     # linear_programming()
-    integer_programming()
+    # integer_programming()
+    bin_packing()
 
 
 def linear_programming():
@@ -88,6 +91,7 @@ def integer_programming():
     # Solve the system.
     status = solver.Solve()
 
+    # Display the solution
     if status == pywraplp.Solver.OPTIMAL:
         objective = solver.Objective()
         assert isinstance(objective, pywraplp.Objective)
@@ -102,6 +106,73 @@ def integer_programming():
     print(f"  Problem solved in {solver.wall_time()} milliseconds")
     print(f"  Problem solved in {solver.iterations()} iterations")
     print(f"  Problem solved in {solver.nodes()} branch-and-bound nodes")
+
+
+def bin_packing():
+    """ Bin-packing sample. """
+
+    data = {}
+    weights = [48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30]
+    data["weights"] = weights
+    data["items"] = list(range(len(weights)))
+    data["bins"] = data["items"]
+    data["bin_capacity"] = 100
+
+    # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    assert isinstance(solver, pywraplp.Solver)
+
+    # Variables
+    # x[i, j] = 1 if item i is packed in bin j.
+    x = {}
+    for i in data["items"]:
+        for j in data["bins"]:
+            x[(i, j)] = solver.IntVar(0, 1, f"x_{i}_{j}")
+
+    # y[j] = 1 if bin j is used.
+    y = {}
+    for j in data["bins"]:
+        y[j] = solver.IntVar(0, 1, f"y[{j}]")
+
+    # Constraints
+    # Each item must be in exactly one bin.
+    for i in data["items"]:
+        solver.Add(sum(x[i, j] for j in data["bins"]) == 1)
+
+    # The amount packed in each bin cannot exceed its capacity.
+    for j in data["bins"]:
+        solver.Add(
+            sum(x[(i, j)] * data["weights"][i]
+                for i in data["items"]) <= y[j] * data["bin_capacity"])
+
+    # Objective: minimize the number of bins used.
+    solver.Minimize(solver.Sum([y[j] for j in data["bins"]]))
+
+    # Solve the system.
+    status = solver.Solve()
+
+    # Display the solution
+    if status == pywraplp.Solver.OPTIMAL:
+        num_bins = 0
+        for j in data["bins"]:
+            if y[j].solution_value() == 1:
+                bin_items = []
+                bin_weight = 0
+                for i in data["items"]:
+                    if x[i, j].solution_value() > 0:
+                        bin_items.append(i)
+                        bin_weight += data["weights"][i]
+                if bin_weight > 0:
+                    num_bins += 1
+                    print("\nBin number", j)
+                    print("  Items packed:", bin_items)
+                    print("  Total weight:", bin_weight)
+
+        print("\nNumber of bins used:", num_bins)
+        print(f"Time = {solver.WallTime()} milliseconds")
+
+    else:
+        print("The problem does not have an optimal solution.")
 
 
 if __name__ == "__main__":
